9f2b906df9b653ebb5cc910699f89bb21d52227c
wwwbian <bdiaz@escuelasproa.edu.ar>
2024-08-26 10:48:10 -0300

    ...

---
+++ examen/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import './App.css'
+import Grilla from './Grilla'
+
+function App() {
+  return (
+    <>
+  
+    <Grilla/>
+    
+    <div className='buttons'>
+    <button className='button'>sumar</button>
+    <button className='button'>restar</button>
+    <button className='button' >mostrar resultados</button>
+    <button className='button' >dividir</button>
+    <button className='button' >multiplicar</button>
+    <button className='button'>limpiar</button>
+    </div>
+    </>
+  )
+}
+
+export default App
---
+++ examen/src/Grilla.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import './App.css'
+
+function Grilla() {
+    const [value, setValue] = useState('');
+
+    const values = (value) => {
+      setValue(((prevValue) => prevValue + value)); 
+    };
+  return (
+
+    <>
+      <input className='input' type="number"  value={value} />
+   <div className="grid-container">
+   <button className="item" onClick={() => values(1)}>1</button>
+      <button className="item" onClick={() => values(2)}>2</button>
+      <button className="item" onClick={() => values(3)}>3</button>
+      <button className="item" onClick={() => values(4)}>4</button>
+      <button className="item" onClick={() => values(5)}>5</button>
+      <button className="item" onClick={() => values(6)}>6</button>
+      <button className="item" onClick={() => values(7)}>7</button>
+      <button className="item" onClick={() => values(8)}>8</button>
+      <button className="item" onClick={() => values(9)} >9</button>
+    </div>
+    </>
+  )
+}
+
+export default Grilla
